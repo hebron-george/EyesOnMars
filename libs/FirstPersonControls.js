107a1e520ebc8ea25492075f4d4a8d33ba048402
Hebron George <hebrontgeorge@gmail.com>
2012-11-11 15:56:55 -0500

    Commented out onkeydown/up event handlers so the user is forced to stay in one spot

--- libs/FirstPersonControls.js
+++ libs/FirstPersonControls.js
@@ -144,61 +144,61 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 		}
 	};
 
-	this.onKeyDown = function ( event ) {
+	// this.onKeyDown = function ( event ) {
 
-		//event.preventDefault();
+	// 	//event.preventDefault();
 
-		switch ( event.keyCode ) {
+	// 	switch ( event.keyCode ) {
 
-			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = true; break;
+	// 		case 38: /*up*/
+	// 		case 87: /*W*/ this.moveForward = true; break;
 
-			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = true; break;
+	// 		case 37: /*left*/
+	// 		case 65: /*A*/ this.moveLeft = true; break;
 
-			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = true; break;
+	// 		case 40: /*down*/
+	// 		case 83: /*S*/ this.moveBackward = true; break;
 
-			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = true; break;
+	// 		case 39: /*right*/
+	// 		case 68: /*D*/ this.moveRight = true; break;
 
-			case 82: /*R*/ this.moveUp = true; break;
-			case 70: /*F*/ this.moveDown = true; break;
+	// 		case 82: /*R*/ this.moveUp = true; break;
+	// 		case 70: /*F*/ this.moveDown = true; break;
 
-			case 81: /*Q*/ this.freeze = !this.freeze; break;
+	// 		case 81: /*Q*/ this.freeze = !this.freeze; break;
 			
-			case 76: /*L*/ 	if( THREEx.FullScreen.activated() ){
-								THREEx.FullScreen.cancel();
-								}else{
-								THREEx.FullScreen.request();
-								} 
+	// 		case 76: /*L*/ 	if( THREEx.FullScreen.activated() ){
+	// 							THREEx.FullScreen.cancel();
+	// 							}else{
+	// 							THREEx.FullScreen.request();
+	// 							} 
 
-		}
+	// 	}
 
-	};
+	// };
 
-	this.onKeyUp = function ( event ) {
+	// this.onKeyUp = function ( event ) {
 
-		switch( event.keyCode ) {
+	// 	switch( event.keyCode ) {
 
-			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = false; break;
+	// 		case 38: /*up*/
+	// 		case 87: /*W*/ this.moveForward = false; break;
 
-			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = false; break;
+	// 		case 37: /*left*/
+	// 		case 65: /*A*/ this.moveLeft = false; break;
 
-			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = false; break;
+	// 		case 40: /*down*/
+	// 		case 83: /*S*/ this.moveBackward = false; break;
 
-			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = false; break;
+	// 		case 39: /*right*/
+	// 		case 68: /*D*/ this.moveRight = false; break;
 
-			case 82: /*R*/ this.moveUp = false; break;
-			case 70: /*F*/ this.moveDown = false; break;
+	// 		case 82: /*R*/ this.moveUp = false; break;
+	// 		case 70: /*F*/ this.moveDown = false; break;
 
-		}
+	// 	}
 
-	};
+	// };
 
 	this.update = function( delta ) {
 		var actualMoveSpeed = 0;
