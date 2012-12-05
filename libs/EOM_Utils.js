6a73d334c27ac57dc2de419bbf0e4bcb0ef94170
benrobot <benrobot@aol.com>
2012-12-04 21:38:27 -0500

    Testing facebook dynamic URL

--- libs/EOM_Utils.js
+++ libs/EOM_Utils.js
@@ -348,4 +348,13 @@ function updateFacebookURL()
 {
 	var sUrl = "http://www.eyesonmars.com/?site=" + currentSite + "&drive=" + currentDrive;
 	document.getElementById('fb').setAttribute('data-href', sUrl);
+	reset_FB_like_button(document, 'script', 'facebook-jssdk');
+}
+
+function reset_FB_like_button(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+	fjs.parentNode.insertBefore(js, fjs);
 }
